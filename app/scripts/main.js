8e2b4a02047806e052d093e345b7339a36fa97d7
Sam Ageloff <sageloff@hugeinc.com>
2015-12-25 22:50:28 -0500

    working on Pivotr, a 'wheel' event/dom interface

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,12 +14,6 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
     }, 2500);
   }
 
-  function debug() {
-    var el = document.createElement('div');
-    el.setAttribute('class', 'debugger');
-    body.appendChild(el);
-  }
-
   // Returns a function, that, as long as it continues to be invoked, will not
   // be triggered. The function will be called after it stops being called for
   // N milliseconds. If `immediate` is passed, trigger the function on the
@@ -39,46 +33,120 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
   	};
   }
 
-  function Pivotr(elem, config) {
-    this.elem = elem;
-    this.config = config;
-    this.currentPos = 0;
-    this.vert = e.deltaY;
+  function Pivotr(opt_config) {
 
-    console.log('Pivotr enabled.');
-  }
-
-  Pivotr.prototype.init = function() {
-
-    window.addEventListener('mousewheel', mousewheel, false);
-
-    var debugConsole = document.querySelector('.debugger');
-
-    debugConsole.innerHTML = [
-      'vert: ', vert, 'currentPos: ', currentPos
+    this.arrays = [];
+    this.triggers = [
+      {"vert_plus_1000": {'elem':'<div class="content-block shadow-depth-3" data-pivotr="[{"addClass":"show", "direction": "vert", "timer" : 1000}, {"removeClass": "show", "direction": "vert", "timer":-1000}]"><img src="https://scontent-ord1-1.xx.fbcdn.net/hphotos-xlp1/t31.0-8/10269131_10151993536642187_4397032661863746536_o.jpg" width="200"></div>', addClass: "show"}},
+      {"vert_minus_1000": {'elem': '<img src="images/triangle-hole.png" alt="" class="triangle-hole" data-pivotr="{&quot;addClass&quot;: &quot;show&quot;, &quot;direction&quot;: &quot;vert&quot;, &quot;timer&quot;: 2000}">', removeClass: "show"}}
     ];
 
-    if (vert > currentPos || vert < currentPos) {
-      currentPos = currentPos + vert;
-      console.log(currentPos, vert);
+    this.map = new Map();
+
+    // Store accumulative X and Y values
+    this.currentPositionX = 0;
+    this.currentPositionY = 0;
+
+    if (opt_config) {
+      this.config = opt_config;
     }
 
-    if (currentPos === 2500) {
-      debugConsole.innerHTML = 'YAY';
+    console.log('Pivotr loaded');
+    this.init_();
+  }
+
+  Pivotr.prototype.view = window;
+
+  Pivotr.prototype.init_ = function() {
+    this.mapTriggers_();
+    this.listen_();
+
+    if (this.config.debug) {
+      this.initDebugger_();
+    }
+  };
+
+  Pivotr.prototype.mapTriggers_ = function() {
+    var data = document.querySelectorAll('[data-pivotr]');
+    var elems = Array.prototype.slice.call(data);
+
+    elems.forEach(function(elem, index, array) {
+      var pivotrs_ = JSON.parse(elem.dataset.pivotr);
+      var pivotr;
+      var len = pivotrs_.length;
+      var i = 0;
+
+      // if there are multiple pivotrs on a node
+      // we need to create a reference to each of them,
+      // and to their parent node
+      if (len) {
+        for (i; i < len; i++) {
+          pivotr = createPivotrObj_(pivotrs_[i], elem);
+        }
+      } else {
+        pivotr = createPivotrObj_(pivotrs_, elem);
+      }
+
+      this.arrays.push(pivotr);
+    }, this);
+
+    function createPivotrObj_(obj, elem) {
+      var temp = {};
+      var objKey = obj.direction + "_" + obj.timer;
+      temp[objKey] = obj;
+      var pivotrObj = temp;
+      return pivotrObj;
     }
 
-    if (currentPos > -2500 && currentPos < -2000) {
-      debugConsole.innerHTML = 'BOO';
+    // this.processMap_();
+  };
+
+  Pivotr.prototype.processMap_ = function() {
+    var temp = [];
+    this.triggers = temp.concat.apply([], this.arrays);
+    console.log(this.triggers);
+  };
+
+  Pivotr.prototype.listen_ = function() {
+    this.view.addEventListener('wheel', function(e) {
+      this.handleEvents_(e);
+    }.bind(this), false);
+  };
+
+  Pivotr.prototype.handleEvents_ = function(e) {
+    var vert = 'vert_' + (e.deltaY < 0) + '_' + Math.abs(e.deltaY);
+    var horz = e.deltaX;
+
+    if (this.triggers) {
+      console.log(vert);
     }
 
-    updateDeltas();
-    e.preventDefault();
+    if (e.deltaY > this.currentPositionY || e.deltaY < this.currentPositionY) {
+      this.currentPositionY = this.currentPositionY + e.deltaY;
+      console.log(this.currentPositionY, e.deltaY);
+    }
 
-    doSomeBasicAnimations();
-    debug();
+    if (e.deltaX > this.currentPositionX || e.deltaX < this.currentPositionX) {
+      this.currentPositionX = this.currentPositionX + e.deltaX;
+      console.log(this.currentPositionX, e.deltaX);
+    }
 
-    var updateDeltas = debounce(function() {
-      console.log('saving position', currentPos);
-    }, 250);
+    this.updateDeltas_();
+  };
 
-};
+  Pivotr.prototype.updateDeltas_ = debounce(function() {
+    console.log('saving position X', this.currentPositionX,
+        'saving position Y', this.currentPositionY);
+  }, 500);
+
+  Pivotr.prototype.initDebugger_ = function() {
+    this.debugConsole = document.createElement('div');
+    this.debugConsole.setAttribute('class', 'debugger');
+    body.appendChild(this.debugConsole);
+  };
+
+  // doSomeBasicAnimations();
+  //
+
+
+var site = new Pivotr({debug:true});
