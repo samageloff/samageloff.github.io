29b2d026f2ad782723e0075bcfcf5d68b40a8ef2
Sam Ageloff <sam@getambassador.com>
2015-12-29 18:14:24 -0500

    working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,7 +40,7 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 
       options:
         // how fast you have to scroll to trigger stuff
-        threshold: low, med, high (default: low)
+        threshold: low (> 20 < 100), med (> 120 < 200), high (> 200) (default: low)
 
         // method you want to trigger on the element
         action: addClass, removeClass, toggleClass (default: addClass)
@@ -52,9 +52,9 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 
     this.arrays = [];
 
-    this.triggers = [
-      {"vert_plus_1000": {'elem':'<div class="content-block shadow-depth-3" data-pivotr="[{"addClass":"show", "direction": "vert", "timer" : 1000}, {"removeClass": "show", "direction": "vert", "timer":-1000}]"><img src="https://scontent-ord1-1.xx.fbcdn.net/hphotos-xlp1/t31.0-8/10269131_10151993536642187_4397032661863746536_o.jpg" width="200"></div>', addClass: "show"}},
-      {"vert_minus_1000": {'elem': '<img src="images/triangle-hole.png" alt="" class="triangle-hole" data-pivotr="{&quot;addClass&quot;: &quot;show&quot;, &quot;direction&quot;: &quot;vert&quot;, &quot;timer&quot;: 2000}">', removeClass: "show"}}
+    this.elems = [
+      {"threshold": "low", "action": "addClass", "className": "show", "direction": "any"},
+      {"threshold": "med", "action": "removeClass", "className": "show", "direction": "up"}
     ];
 
     this.map = new Map();
@@ -63,9 +63,7 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
     this.topics = {};
     this.subUid = -1;
 
-    // Store accumulative X and Y values
-    this.currentPositionX = 0;
-    this.currentPositionY = 0;
+    this.thresholds = ['low', 'med', 'high'];
 
     if (opt_config) {
       this.config = opt_config;
@@ -77,33 +75,35 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 
   Pivotr.prototype.view = window;
 
+
   Pivotr.prototype.init_ = function() {
-    this.mapTriggers_();
+    this.mapElems_();
     this.listen_();
   };
 
-  Pivotr.prototype.mapTriggers_ = function() {
-    var data = document.querySelectorAll('[data-pivotr]');
-    var elems = Array.prototype.slice.call(data);
+
+  Pivotr.prototype.mapElems_ = function() {
+    var attrs = document.querySelectorAll('[data-pivotr]');
+    var elems = Array.prototype.slice.call(attrs);
 
     elems.forEach(function(elem, index, array) {
-      var pivotrs_ = JSON.parse(elem.dataset.pivotr);
-      var pivotr;
-      var len = pivotrs_.length;
+      var attrs_ = JSON.parse(elem.dataset.pivotr);
+      var attr;
+      var len = attrs_.length;
       var i = 0;
 
-      // if there are multiple pivotrs on a node
+      // if there are multiple attributes on a node
       // we need to create a reference to each of them,
       // and to their parent node
       if (len) {
         for (i; i < len; i++) {
-          pivotr = createPivotrObj_(pivotrs_[i], elem);
+          attr = createPivotrObj_(attrs_[i], elem);
         }
       } else {
-        pivotr = createPivotrObj_(pivotrs_, elem);
+        attr = createPivotrObj_(attrs_, elem);
       }
 
-      this.arrays.push(pivotr);
+      this.arrays.push(attr);
     }, this);
 
     function createPivotrObj_(obj, elem) {
@@ -123,37 +123,28 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
     console.log(this.triggers);
   };
 
+
   Pivotr.prototype.listen_ = function() {
     this.view.addEventListener('wheel', function(e) {
       this.handleEvents_(e);
     }.bind(this), false);
   };
 
+
   Pivotr.prototype.handleEvents_ = function(e) {
-    var vert = 'vert_' + (e.deltaY < 0) + '_' + Math.abs(e.deltaY);
+    var vert = e.deltaY;
     var horz = e.deltaX;
 
-    if (this.triggers) {
-      console.log(vert);
-    }
-
-    if (e.deltaY > this.currentPositionY || e.deltaY < this.currentPositionY) {
-      this.currentPositionY = this.currentPositionY + e.deltaY;
-      console.log(this.currentPositionY, e.deltaY);
-    }
-
-    if (e.deltaX > this.currentPositionX || e.deltaX < this.currentPositionX) {
-      this.currentPositionX = this.currentPositionX + e.deltaX;
-      console.log(this.currentPositionX, e.deltaX);
-    }
+    console.log(horz, vert);
 
     this.updateDeltas_();
   };
 
+
   Pivotr.prototype.updateDeltas_ = debounce(function() {
-    console.log('saving position X', this.currentPositionX,
-        'saving position Y', this.currentPositionY);
+    console.log('stopped scrolling for now');
   }, 500);
+
 
   Pivotr.prototype.subscribe = function(topic, func) {
     if (!this.topics[topic]) {
@@ -166,6 +157,7 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
     });
     return token;
   };
+
 
   Pivotr.prototype.publish = function(topic, args) {
     if (!this.topics[topic]) {
@@ -182,6 +174,7 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
     return true;
   };
 
+
   Pivotr.prototype.unsubscribe = function(token) {
     for (var m in this.topics) {
       if (this.topics[m]) {
@@ -195,6 +188,7 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
     }
     return false;
   };
+
 
   doSomeBasicAnimations();
 
